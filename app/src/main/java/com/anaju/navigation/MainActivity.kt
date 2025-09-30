d605429ef9fdd2a05e942fa3e16bf96f43adc088
AnaJu <anajuboechat@gmail.com>
2025-09-30 10:36:09 -0300

    final

--- app/src/main/java/com/anaju/navigation/MainActivity.kt
+++ app/src/main/java/com/anaju/navigation/MainActivity.kt
@@ -2,20 +2,22 @@ package com.anaju.navigation
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.core.view.GravityCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.anaju.navigation.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var fragmentManager: FragmentManager
+    private val fragmentManager: FragmentManager by lazy { supportFragmentManager }
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,12 +43,52 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        // setSupportActionBar?.title = "" // descomente se quiser remover o título
+        binding.navView.setNavigationItemSelectedListener(this)
 
-        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+        // BottomNavigation
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_cart -> openFragment(CarrinhoFragment())
+                R.id.bottom_profile -> openFragment(PerfilFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categorias", Toast.LENGTH_SHORT).show()
+        }
+
+        // Back button
+        onBackPressedDispatcher.addCallback(this) {
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                finish()
+            }
+        }
     }
 
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        TODO("Not yet implemented")
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_eletronico -> openFragment(EletronicoFragment())
+            R.id.nav_beleza -> openFragment(BelezaFragment())
+            R.id.nav_alimentos -> openFragment(AlimentosFragment())
+            R.id.nav_casa -> openFragment(CasaFragment())
+            R.id.nav_livros -> openFragment(LivrosFragment())
+            R.id.nav_brinquedos -> openFragment(BrinquedoFragment())
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
